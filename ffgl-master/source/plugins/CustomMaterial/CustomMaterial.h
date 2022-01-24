ed2507250f9b36f0b3afe693aa59bcb09cbee895
rayokuma <53759287+rayokuma@users.noreply.github.com>
2022-01-25 00:32:40 +0100

    Update CustomMaterial.h

--- ffgl-master/source/plugins/CustomMaterial/CustomMaterial.h
+++ ffgl-master/source/plugins/CustomMaterial/CustomMaterial.h
@@ -21,13 +21,11 @@ private:
 	//parameters
 	int unreal_params_material;
 	
-	float specular_roughness;				//!< In range between 0.0 .. 1.0		
-	float metalness;				//!< In range between -180.0 .. 180.0	
+	float specular_roughness;	//!< In range between 0.0 .. 1.0		
+	float metalness;			//!< In range between -180.0 .. 180.0	
 	float emission;				//!< In range between -1500.0 .. -1000.0
 	float rgba[ 4 ];
 
 	ffglex::FFGLShader shader;  //!< Utility to help us compile and link some shaders into a program.
 	ffglex::FFGLScreenQuad quad;//!< Utility to help us render a full screen quad.
 };
-
-
